78bc0400f7aa6c4ca044acf78e9c8720c6a85f65
Catherine Kim <abbytherabbit@gmail.com>
2020-07-29 13:41:38 -0700

    adding files for sorting

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log('test change');
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
+
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+const sorter = new Sorter(numbersCollection);
+sorter.sort();
+console.log(numbersCollection.data);
